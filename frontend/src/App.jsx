999a09a6af96a718c7ea6aa55bb2e99e21ce3156
Aleksandr Shenshin <shenshin@me.com>
2021-03-09 17:15:55 +0100

    Admin screens - Part I

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,8 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
+import UserEditScreen from './screens/UserEditScreen';
 
 const App = () => (
   <Router>
@@ -29,6 +31,8 @@ const App = () => (
         <Route path="/product/:id" component={ProductScreen} />
         {/* ? means that id is optional */}
         <Route path="/cart/:id?" component={CartScreen} />
+        <Route path="/admin/userlist" component={UserListScreen} />
+        <Route path="/admin/user/:id/edit" component={UserEditScreen} />
         <Route path="/" component={HomeScreen} exact />
       </Container>
     </main>
